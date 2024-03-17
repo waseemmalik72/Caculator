9ac93e0b0c5723d6c5317b30fa9f7b64d6b36378
Dawoodali007 <dawoodalishedazy1212@gmail.com>
2024-03-17 16:22:01 -0700

    all clearr done

--- Caculator.js
+++ Caculator.js
@@ -2,11 +2,14 @@ var display = document.getElementById("total");
 function dependOnclIck(input) {
     display.value += input;
 };
+function AllcLear() {
+    display.value = "";
+};
 function cacuLate() {
     try {
-        display.value = eval(display.value)
+        display.value = eval(display.value);
     } catch (error) {
-        display.value = "Error"
-    }
+        display.value = "Error";
+    };
     
-}
+};
